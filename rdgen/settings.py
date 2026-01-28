c1af0a3a272e1074ee7ca342a94fe2e01c226d2d
Alexander Kosko <dayanger@gmail.com>
2026-01-28 11:53:46 +0300

    Fix env.compname in generator-windows.yml Add run.bat for windows Fix .gitignore In settings.py add load from .env

--- rdgen/settings.py
+++ rdgen/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv('.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +34,10 @@ REPONAME = os.environ.get("REPONAME", 'rdgen')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+print("--------------")
+print(GHUSER)
+print(GENURL)
+print("--------------")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
